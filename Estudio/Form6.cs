bbfbeee51b356b13b2f8aadf60e64625776e85fc
maria <maria@MARIANA-NTB>
2023-09-25 16:14:33 -0300

    Adicionar arquivos de projeto.

---
+++ Estudio/Form6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estudio
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            float preco = float.Parse(txtPreco.Text);
+            int qtd_alunos = int.Parse(txtAlunos.Text);
+            int qtd_aulas = int.Parse(txtAulas.Text);
+            Modalidade modalidade = new Modalidade(txtDescricao.Text,preco,qtd_alunos,qtd_aulas);
+            if (modalidade.cadastrarModalidade())
+                MessageBox.Show("Cadastro realizado com sucesso!");
+            else
+                MessageBox.Show("Erro no cadastro!");
+        }
+    }
+}
---
+++ Estudio/Form7.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estudio
+{
+    public partial class Form7 : Form
+    {
+        public Form7()
+        {
+            InitializeComponent();
+            Modalidade exc = new Modalidade();
+            MySqlDataReader r = exc.consultarTodasModalidade();
+            while(r.Read())
+            {
+                cbxDescricao.Items.Add(r["descricaoModalidade"].ToString());
+            }
+            DAO_Conexao.con.Close();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            Modalidade modalidade = new Modalidade();
+            modalidade.excluirModalidade(cbxDescricao.Text);
+            
+            
+        }
+
+        private void Form7_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
